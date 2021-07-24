8eee421caf91e1a0629f51b62e363eaa13a3672d
ClaraPasman <85891515+ClaraPasman@users.noreply.github.com>
2021-07-24 18:25:12 -0300

    Reorder and Add comments

--- Modelo4cPPedit.py
+++ Modelo4cPPedit.py
@@ -1,9 +1,5 @@
-"""
-Model exported as python.
-Name : modelo4c
-Group : 
-With QGIS : 31608
-"""
+########Prepare QGIS for working########
+##################################################################################################################
 
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
@@ -22,13 +18,12 @@ class Modelo4c(QgsProcessingAlgorithm):
         self.addParameter(QgsProcessingParameterFeatureSink('Contries_drop_fields', 'contries_drop_fields', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
 
     def processAlgorithm(self, parameters, context, model_feedback):
-        # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
-        # overall progress through the model
+      
         feedback = QgsProcessingMultiStepFeedback(5, model_feedback)
         results = {}
         outputs = {}
-        
-         # Quitar campo(s)
+##################################################################################################################       
+#######Drop variables that you're not interested in#######
         alg_params = {
             'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','APCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','GDP_MD_EST','POP_YEAR','LASTCENSUS','GDP_YEAR','ECONOMY','INCOME_GRP','WIKIPEDIA','FIPS_10_','ISO_A2','ISO_A3_EH','ISO_N3','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_A3_IS','ADM0_A3_US','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FR','NAME_EL','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_VI','NAME_ZH','MAPCOLOR9\n'],
             'INPUT': '/Users/rochipodesta/Desktop/maestría/Herramientas/semana 5/input/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp',
@@ -38,8 +33,7 @@ class Modelo4c(QgsProcessingAlgorithm):
         results['Contries_drop_fields'] = outputs['QuitarCampos']['OUTPUT']
         return results
 
-
-        # Reproyectar capa
+ ######## Reproject layer #######
         alg_params = {
             'INPUT': 'Campos_restantes_45ce4415_6464_467e_97d8_264f642604de',
             'OPERATION': '',
@@ -53,22 +47,8 @@ class Modelo4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Save vector features to file
-        alg_params = {
-            'DATASOURCE_OPTIONS': '',
-            'INPUT': 'Calculado_cc92d7b8_209b_4a65_a545_70dbfabe82e5',
-            'LAYER_NAME': '',
-            'LAYER_OPTIONS': '',
-            'OUTPUT': '/Users/rochipodesta/Desktop/maestría/Herramientas/semana 5/output/areas.csv',
-            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
-        }
-        outputs['SaveVectorFeaturesToFile'] = processing.run('native:savefeatures', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-
-        feedback.setCurrentStep(3)
-        if feedback.isCanceled():
-            return {}
-
-        # Corregir geometrías
+     
+####### Fix geometries for country polygons: use this tool to fix any polygon g
         alg_params = {
             'INPUT': 'Reproyectada_6d9a07cc_6bf4_424b_9706_32706b7f2cf5',
             'OUTPUT': parameters['Countries_fixgeo']
@@ -80,7 +60,7 @@ class Modelo4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
         
-        # Calculadora de campos
+####### Field Calculator: calculate areas#######
         alg_params = {
             'FIELD_LENGTH': 10,
             'FIELD_NAME': 'km2area',
@@ -94,6 +74,22 @@ class Modelo4c(QgsProcessingAlgorithm):
         results['Areas_out'] = outputs['CalculadoraDeCampos']['OUTPUT']
 
         feedback.setCurrentStep(1)
+        if feedback.isCanceled():
+            return {}
+        
+        
+######## Save vector features to file ####### 
+        alg_params = {
+            'DATASOURCE_OPTIONS': '',
+            'INPUT': 'Calculado_cc92d7b8_209b_4a65_a545_70dbfabe82e5',
+            'LAYER_NAME': '',
+            'LAYER_OPTIONS': '',
+            'OUTPUT': '/Users/rochipodesta/Desktop/maestría/Herramientas/semana 5/output/areas.csv',
+            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+        }
+        outputs['SaveVectorFeaturesToFile'] = processing.run('native:savefeatures', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+
+        feedback.setCurrentStep(3)
         if feedback.isCanceled():
             return {}
 

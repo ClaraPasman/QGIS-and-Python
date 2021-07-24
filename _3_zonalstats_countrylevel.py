816128890e85a1fcd88e11ed0a92a6614b0f9095
ClaraPasman <85891515+ClaraPasman@users.noreply.github.com>
2021-07-24 17:43:49 -0300

    Delete extra information and add comments

--- _3_zonalstats_countrylevel.py
+++ _3_zonalstats_countrylevel.py
@@ -1,37 +1,37 @@
-#########################################################################################
-#########################################################################################
-# SETUP PREAMBLE FOR RUNNING STANDALONE SCRIPTS.
-# NOT NECESSARY IF YOU ARE RUNNING THIS INSIDE THE QGIS GUI.
-# print('preliminary setup')
-# import sys
-# import os
-
-# from qgis.core import (
-#     QgsApplication
-# )
-
-# from qgis.analysis import QgsNativeAlgorithms
-
-# # See https://gis.stackexchange.com/a/155852/4972 for details about the prefix 
-# QgsApplication.setPrefixPath('C:/OSGeo4W64/apps/qgis', True)
-# qgs = QgsApplication([], False)
-# qgs.initQgis()
-
-# # Add the path to Processing framework  
-# sys.path.append('C:/OSGeo4W64/apps/qgis/python/plugins')
-
-# # Import and initialize Processing framework
-# import processing
-# from processing.core.Processing import Processing
-# Processing.initialize()
-# QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
-#########################################################################################
+####### Prepare QGIS for working#######
 #########################################################################################
 
-# set paths to inputs and outputs
-mainpath = "/Users/magibbons/Desktop/Herramientas/Clase5/input"
+print('preliminary setup')
+import sys
+import os
+
+ from qgis.core import (
+     QgsApplication
+ )
+
+ from qgis.analysis import QgsNativeAlgorithms
+
+
+ QgsApplication.setPrefixPath('C:/OSGeo4W64/apps/qgis', True)
+ qgs = QgsApplication([], False)
+ qgs.initQgis()
+
+ sys.path.append('C:/OSGeo4W64/apps/qgis/python/plugins')
+
+
+import processing
+from processing.core.Processing import Processing
+Processing.initialize()
+QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
+#########################################################################################
+
+
+
+####### Set paths to inputs and outputs#######
+mainpath = "/Users/clara/Documents/MAESTRIA/HERRAMIENTAS COMPUTACIONALES/PYTHON-QGGIS 2"
 outpath = "{}/_output".format(mainpath)
 
+#######Import rasters with the information required#######
 elevation = "D:/backup_school/Research/IPUMS/_GEO/elrug/elevation/alt.bil"
 tpbasepath = "D:/backup_school/Research/worldclim/World"
 tpath = tpbasepath + "/temperature"
@@ -46,13 +46,11 @@ countries = mainpath + "/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp"
 
 outcsv = "{}/country_level_zs.csv".format(outpath)
 
+
 RASTS = [elevation, temp, prec, landqual, popd1500, popd1990, popd2000]
 PREFS = ['elev_', 'temp_', 'prec_', 'lqua_', 'pd15_', 'pd19_', 'pd20_']
 
-# elevation, temperature, precipitation are very large raster files,
-# take a long time to process. we will see faster processing methods at the end. 
-# the code will still run (if you are patient)!
-# for now, do only the last four rasters
+#######Only use last 4 rasters, due to processing time requirements#######
 RASTS = RASTS[3:]
 PREFS = PREFS[3:]
 

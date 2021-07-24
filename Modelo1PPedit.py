3e6a6533ea007f9d20dcf3a9e8d5a3dbb77cbe81
ClaraPasman <85891515+ClaraPasman@users.noreply.github.com>
2021-07-24 17:31:33 -0300

    Reorder and delete extra data

--- Modelo1PPedit.py
+++ Modelo1PPedit.py
@@ -1,9 +1,4 @@
-"""
-Model exported as python.
-Name : Modelo1
-Group : 
-With QGIS : 31608
-"""
+
 
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
@@ -29,20 +24,43 @@ class Modelo1(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
-        # Quitar campo(s)
+        
+        
+
+        
+        
         alg_params = {
-            'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length\n'],
-            'INPUT': 'Calculado_96c74cd1_8dc9_4738_8943_be3f054c5463',
-            'OUTPUT': parameters['Wldsout']
+            'INPUT': '/Users/rochipodesta/Desktop/maestría/Herramientas/semana 5/langa/langa.shp',
+            'OUTPUT': parameters['Fix_geo']
         }
-        outputs['QuitarCampos'] = processing.run('qgis:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Wldsout'] = outputs['QuitarCampos']['OUTPUT']
+        outputs['CorregirGeometras'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Fix_geo'] = outputs['CorregirGeometras']['OUTPUT']
 
-        feedback.setCurrentStep(1)
+        feedback.setCurrentStep(5)
         if feedback.isCanceled():
-            return {}
+            return {}       
+        
+        
+        
+        alg_params = {
+            'FIELD_NAME': 'GID',
+            'GROUP_FIELDS': [''],
+            'INPUT': outputs['CorregirGeometras']['OUTPUT'],
+            'SORT_ASCENDING': True,
+            'SORT_EXPRESSION': '',
+            'SORT_NULLS_FIRST': False,
+            'START': 1,
+            'OUTPUT': parameters['Autoinc_id']
+        }
+        outputs['AgregarCampoQueAutoincrementa'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Autoinc_id'] = outputs['AgregarCampoQueAutoincrementa']['OUTPUT']
+        return results
 
-        # Calculadora de campos (clone)
+        
+        
+        
+        
+             
         alg_params = {
             'FIELD_LENGTH': 10,
             'FIELD_NAME': 'lnm',
@@ -58,6 +76,41 @@ class Modelo1(QgsProcessingAlgorithm):
         feedback.setCurrentStep(2)
         if feedback.isCanceled():
             return {}
+
+        
+   
+        alg_params = {
+            'INPUT': 'Calculado_4b2727ef_a5b8_48af_9cc3_438be5c0002e',
+            'OUTPUT_menor_a_11': parameters['Output_menor_a_11']
+        }
+        outputs['FiltroDeEntidad'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Output_menor_a_11'] = outputs['FiltroDeEntidad']['OUTPUT_menor_a_11']
+
+        feedback.setCurrentStep(4)
+        if feedback.isCanceled():
+            return {}
+        
+        
+        
+       
+      
+        alg_params = {
+            'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length\n'],
+            'INPUT': 'Calculado_96c74cd1_8dc9_4738_8943_be3f054c5463',
+            'OUTPUT': parameters['Wldsout']
+        }
+        outputs['QuitarCampos'] = processing.run('qgis:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Wldsout'] = outputs['QuitarCampos']['OUTPUT']
+
+        feedback.setCurrentStep(1)
+        if feedback.isCanceled():
+            return {}
+
+   
+
+
+
+
 
         # Calculadora de campos
         alg_params = {
@@ -76,45 +129,11 @@ class Modelo1(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Filtro de entidad
-        alg_params = {
-            'INPUT': 'Calculado_4b2727ef_a5b8_48af_9cc3_438be5c0002e',
-            'OUTPUT_menor_a_11': parameters['Output_menor_a_11']
-        }
-        outputs['FiltroDeEntidad'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Output_menor_a_11'] = outputs['FiltroDeEntidad']['OUTPUT_menor_a_11']
+       
 
-        feedback.setCurrentStep(4)
-        if feedback.isCanceled():
-            return {}
 
-        # Corregir geometrías
-        alg_params = {
-            'INPUT': '/Users/rochipodesta/Desktop/maestría/Herramientas/semana 5/langa/langa.shp',
-            'OUTPUT': parameters['Fix_geo']
-        }
-        outputs['CorregirGeometras'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Fix_geo'] = outputs['CorregirGeometras']['OUTPUT']
-
-        feedback.setCurrentStep(5)
-        if feedback.isCanceled():
-            return {}
-
-        # Agregar campo que auto-incrementa 
-        alg_params = {
-            'FIELD_NAME': 'GID',
-            'GROUP_FIELDS': [''],
-            'INPUT': outputs['CorregirGeometras']['OUTPUT'],
-            'SORT_ASCENDING': True,
-            'SORT_EXPRESSION': '',
-            'SORT_NULLS_FIRST': False,
-            'START': 1,
-            'OUTPUT': parameters['Autoinc_id']
-        }
-        outputs['AgregarCampoQueAutoincrementa'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Autoinc_id'] = outputs['AgregarCampoQueAutoincrementa']['OUTPUT']
-        return results
-
+    
+  
     def name(self):
         return 'Modelo1'
 
